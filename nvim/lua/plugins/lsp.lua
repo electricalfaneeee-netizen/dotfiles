36e3889b1c113beac9d37fd85634fcf0d62db409
coffeecrisp <electricalfaneeee@gmail.com>
2026-09-12 18:00:34 -0400

    add Hyprland Lua config auto-completion

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,7 +6,20 @@ return {
 
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 	
-	vim.lsp.config("lua_ls", { capabilities = capabilities })
+	vim.lsp.config("lua_ls", {
+	    capabilities = capabilities,
+	    settings = {
+		Lua = {
+		    workspace = {
+			library = {
+			    vim.fn.expand("/usr/share/hypr/stubs/hl.meta.lua"),
+			},
+			checkThirdParty = false,
+		    },
+		    telemetry = { enable = false },
+		},
+	    },
+	})
 	vim.lsp.config("pyright", { capabilities = capabilities })
 	
 	vim.lsp.enable("lua_ls")
